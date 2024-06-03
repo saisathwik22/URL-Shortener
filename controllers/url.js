64f640f28d5453c2c239eedb7da750a58bb1401b
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-06-04 01:36:41 +0530

    middleware, routes, service, ejs views, controllers, models.

--- controllers/url.js
+++ controllers/url.js
@@ -11,9 +11,12 @@ async function handleGenerateNewShortURL(req, res) {
     shortId: shortID,
     redirectURL: body.url,
     visitedHistory: [],
+    createdBy: req.user._id,
   });
 
-  return res.json({ id: shortID });
+  return res.render("home", {
+    id: shortID,
+  });
 }
 
 async function handleGetAnalytics(req, res) {
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 const { connectToMongoDB } = require("./connect");
-const urlRoute = require("./routes/url");
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth");
+
 const URL = require("./models/url");
+
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
 
 const app = express();
 const PORT = 8001;
@@ -10,12 +17,19 @@ connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
   console.log("MongoDb connected")
 );
 
-app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
-app.use("/url", urlRoute);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
 
 // when data fetched using shortid.
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
