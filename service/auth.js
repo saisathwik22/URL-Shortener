3fec477573932ab8ad84840511e90c9d5a4ecc0a
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-06-04 11:24:40 +0530

    JWT Authentication

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,27 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "Piyush$123@$";
 
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+/* This function generates tokens 
+  This token when pasted in jwt.io displays the payload mapped with it.
+*/
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+/* To verify the token with our secretKey */
+function getUser(token) {
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports = {
